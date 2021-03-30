7d864e4f21d7a5a3b373c71509ada9c2b9ec28f1
tahamousavi <tahamousavi.sbu@gmail.com>
2021-03-30 15:07:54 +0430

    State provider with single product

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,7 +5,6 @@ import '../providers/products.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -13,10 +12,13 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-            products[i].id,
-            products[i].title,
-            products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+            create: (c) => products[i],
+            child: ProductItem(
+                // products[i].id,
+                // products[i].title,
+                // products[i].imageUrl,
+                ),
           ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
